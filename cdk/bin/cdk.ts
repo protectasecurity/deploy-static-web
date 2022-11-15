38a2d9d7e54641f93dc8f4492cf33bea2043586f
Ronnie Ayala <ronnie.ayala@outlook.com>
2022-11-15 17:00:08 -0600

    add short hash support

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+// @ts-ignore
+import * as sh from 'shorthash';
 import { CdkStack } from '../lib/cdk-stack';
 
 const repository = process.env.GITHUB_REPOSITORY;
@@ -18,9 +20,15 @@ if(!refname) {
 
 const app = new cdk.App();
 
+let stackName = repository.split('/')[1];
+const customDomain = process.env.INPUT_DOMAIN;
+if(customDomain) {
+  const hash = sh.unique(customDomain);
+  stackName = `${stackName}-${hash}`;
+}
 
 new CdkStack(app, 'StaticWebStack', {
-  stackName: repository.split('/')[1],
+  stackName: stackName,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
